dbb1297c971a4da609e1da056d56873fd45b4739
usagibravo <usagibravo@gmail.com>
2023-04-17 11:02:50 +0000

    04 kadai completion

---
+++ 04/month.js
@@ -0,0 +1,28 @@
+let month = promot()
+if (month === 1) {
+  alert('January');
+} else if (month === 2 ) {
+  alert('Feburary');
+} else if (month === 3) {
+  alert('Feburary');
+} else if (month === 4) {
+  alert('Feburary');
+} else if (month === 5) {
+  alert('Feburary');
+} else if (month === 6) {
+  alert('Feburary');
+} else if (month === 7) {
+  alert('Feburary');
+} else if (month === 8) {
+  alert('Feburary');
+} else if (month === 9) {
+  alert('Feburary');
+} else if (month === 10) {
+  alert('Feburary');
+} else if (month === 11) {
+  alert('Feburary');
+} else if (month === 12) {
+  alert('Feburary');
+} else {
+  alert('判定できません。')
+}

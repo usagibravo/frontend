61c5f7aaf35cc9fd9cdaf1ad9ffcbac9d765862a
usagibravo <usagibravo@gmail.com>
2023-04-17 12:24:01 +0000

    06 kadai completion

---
+++ 06/kadai06.js
@@ -0,0 +1,7 @@
+let fruit = ['apricot', 'strawberry'];
+// console.log(fruit);
+fruit.unshift('peer');
+fruit.push('grape');
+// console.log(fruit);
+fruit[fruit.length-1] = 'lime';
+console.log(fruit);

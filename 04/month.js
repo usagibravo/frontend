5e761038ceb88adcc9c61698bb41d73e9c28b2b6
usagibravo <usagibravo@gmail.com>
2023-04-17 11:15:48 +0000

    04 kadai final

--- 04/month.js
+++ 04/month.js
@@ -1,27 +1,27 @@
 let month = prompt();
-if (month === 1) {
+if (month == 1) {
   alert('January');
-} else if (month === 2 ) {
+} else if (month == 2 ) {
   alert('Feburary');
-} else if (month === 3) {
+} else if (month == 3) {
   alert('March');
-} else if (month === 4) {
+} else if (month == 4) {
   alert('April');
-} else if (month === 5) {
+} else if (month == 5) {
   alert('May');
-} else if (month === 6) {
+} else if (month == 6) {
   alert('June');
-} else if (month === 7) {
+} else if (month == 7) {
   alert('July');
-} else if (month === 8) {
+} else if (month == 8) {
   alert('August');
-} else if (month === 9) {
+} else if (month == 9) {
   alert('September');
-} else if (month === 10) {
+} else if (month == 10) {
   alert('October');
-} else if (month === 11) {
+} else if (month == 11) {
   alert('November');
-} else if (month === 12) {
+} else if (month == 12) {
   alert('December');
 } else {
   alert('不明です。')

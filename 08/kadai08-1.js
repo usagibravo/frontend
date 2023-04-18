4bea2dbd45dab334f2e8bfc3cc5c56bd530dd1aa
Masahiro Ohno <usagibravo@gmail.com>
2023-04-18 15:04:12 +0900

    08 kadai compoletion

---
+++ 08/kadai08-1.js
@@ -0,0 +1,4 @@
+console.log('高さ２０底辺３０の面積（関数名なし）：' + (function(height, width){
+  area = height * width / 2
+  return area
+})(20, 30));
---
+++ 08/kadai08-2.js
@@ -0,0 +1,6 @@
+function calcArea(height, width) {
+    area = height * width /2;
+    return area
+}
+
+console.log('高さ２０底辺３０の面積（関数名あり）：' + calcArea(20, 30));

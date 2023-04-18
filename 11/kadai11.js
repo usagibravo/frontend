685e9690cd5a641f83d5d2d525b3480306f6cd9a
Masahiro Ohno <usagibravo@gmail.com>
2023-04-18 19:28:35 +0900

    11 kadai completion

---
+++ 11/kadai11.js
@@ -0,0 +1,7 @@
+let e1 = document.getElementById('p1');
+let e2 = document.getElementById('p2');
+let e3 = document.getElementById('p3');
+
+e1.outerHTML = '<input type="text" name="name" required></input>';
+e2.innerText = 'テキスト文字列';
+e3.id = 'p5';

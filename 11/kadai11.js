c7832bc286d0d5b1fd3bd5554fa89291909a4208
Masahiro Ohno <usagibravo@gmail.com>
2023-04-19 19:41:52 +0900

    11 kadai fix

--- 11/kadai11.js
+++ 11/kadai11.js
@@ -2,6 +2,6 @@ let e1 = document.getElementById('p1');
 let e2 = document.getElementById('p2');
 let e3 = document.getElementById('p3');
 
-e1.outerHTML = '<input type="text" name="name" required></input>';
+e1.outerHTML = '<input type="text" name="name" required>';
 e2.innerText = 'テキスト文字列';
 e3.id = 'p5';
